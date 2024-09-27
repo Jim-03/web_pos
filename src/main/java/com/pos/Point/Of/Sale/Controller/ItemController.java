646daf410ac8c7b7a639f422f13b13a50436fcd3
Jim-03 <jimmycn03@gmail.com>
2024-09-27 12:17:03 +0300

    Added a parameter to simplify updating process

--- src/main/java/com/pos/Point/Of/Sale/Controller/ItemController.java
+++ src/main/java/com/pos/Point/Of/Sale/Controller/ItemController.java
@@ -1,11 +1,9 @@
 package com.pos.Point.Of.Sale.Controller;
 
 import com.pos.Point.Of.Sale.CustomResponse;
-import com.pos.Point.Of.Sale.Entity.Category;
 import com.pos.Point.Of.Sale.Entity.Item;
 import com.pos.Point.Of.Sale.Service.CategoryService;
 import com.pos.Point.Of.Sale.Service.ItemService;
-import com.pos.Point.Of.Sale.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,8 +42,13 @@ public class ItemController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity<CustomResponse> updateItem(@RequestBody Item item) {
-        CustomResponse response = service.updateItem(item);
+    public ResponseEntity<CustomResponse> updateItem(@RequestBody Item item, @RequestParam String initialName, @RequestParam String newCategoryName) {
+        CustomResponse category = categoryService.getCategory(newCategoryName);
+        if (category.getCategory() == null) {
+            return new ResponseEntity<>(category, HttpStatus.NOT_FOUND);
+        }
+        item.setCategory(category.getCategory());
+        CustomResponse response = service.updateItem(item, initialName);
         return switch (response.getStatus()) {
             case REJECTED -> new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
             case ABSENT -> new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
@@ -61,6 +64,16 @@ public class ItemController {
             case REJECTED -> new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
             case ABSENT -> new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
             case SUCCESS -> new ResponseEntity<>(response, HttpStatus.OK);
+            default -> new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        };
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<CustomResponse> findAll(@RequestParam Long categoryId) {
+        CustomResponse response = service.getAll(categoryId);
+        return switch (response.getStatus()) {
+            case SUCCESS -> new ResponseEntity<>(response, HttpStatus.OK);
+            case ABSENT -> new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
             default -> new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         };
     }
